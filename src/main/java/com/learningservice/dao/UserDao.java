1d1766a1cc5d1963f2f4aeda88cf7dc16aae7d00
Rashid Jilani <rashid.jilani@gmail.com>
2021-03-08 09:58:47 -0600

    changed stereotype to Repository for DAO

--- src/main/java/com/learningservice/dao/UserDao.java
+++ src/main/java/com/learningservice/dao/UserDao.java
@@ -6,13 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Service;
-
+import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-@Service
+@Repository
 public class UserDao implements IUserDao{
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
